a2e7d95caf0d025a6652d2656978c67f31eec2bf
Isaac Castaneda <isaac.audioeng@gmail.com>
2016-07-30 01:45:31 -0400

    update1

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 module.exports = function Route(app, server){
 	var io = require('socket.io').listen(server)
   var history = [];
+	var name = '';
 	io.sockets.on('connection', function (socket) {
-    socket.on('clear_messages', function(){
+    socket.on('clear_messages', function(data){
       history = [];
-      io.emit('clear_out');
+			io.emit('clear_out');
+      socket.broadcast.emit('cleared', {response:data.name});
     })
     socket.on("new_message", function (data){
       history.push(data.message);
       io.emit('receive_message', {response:data.message});
     })
     socket.on('new_user', function(data){
+			name = data.name;
       socket.emit('history', {response:history});
-      socket.broadcast.emit('user_login', {response:data.name} )
+      socket.broadcast.emit('user_login', {response:data.name})
     })
+		socket.on('disconnect', function(){
+			socket.broadcast.emit('user_logout', {response:name})
+		})
 	})
+
 	// root route to render the index.ejs view
 	app.get('/', function(req, res) {
 	 res.render("index");
